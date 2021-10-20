5d4750e60c5085d85cccac20b043729a53e53576
ikmal <ikmalazman65@gmail.com>
2021-10-20 23:45:59 +0000

    Differentiate parent and child

---
+++ Q2.c
@@ -0,0 +1,23 @@
+#include <stdlib.h> /* needed to define exit() */
+#include <unistd.h> /* needed for fork() and getpid() */
+#include <stdio.h> /* needed for print */
+
+int main(int argc, char **argv){
+
+int pid; /*processID*/
+
+
+switch (pid = fork()){
+
+case 0: printf("I am the child process : pid = %d \n " , getpid());
+	break ; /* a fork returns 0 to the child */
+default: printf(" I am the parent process : pid = %d,child pid=%d \n ",getpid(),pid);
+	break; /* a fork returns a pid to the parent */
+case -1 : perror ("fork");
+	exit(1); /* Something went wrong */
+}
+
+exit(0);
+
+}
+
