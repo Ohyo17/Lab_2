0dc10dc8cc2a42bc64569d64e9c3a67ad9d0ef51
ikmal <ikmalazman65@gmail.com>
2021-10-21 13:15:05 +0000

    Function

--- function.c
+++ function.c
@@ -18,11 +18,13 @@ pid_t pid = fork ();
 
 if (pid == 0){
 childTask();
+printf("Child process: PPID=%d , PID=%d \n " , getppid(),getpid());
 exit(EXIT_SUCCESS);
 }
 else if (pid > 0){
 wait(NULL);
 parentTask();
+printf("Parent Process : PID=%d \n", getpid());
 }
 else {
 printf("Unable to create child process\n");
